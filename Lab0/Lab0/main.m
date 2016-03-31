4704f851950681a7bf311c73f7e2627fb2822206
BrendanSChang <BrendanSChang@gmail.com>
2016-03-31 13:29:20 -0400

    Restored Lab0 code

---
+++ Lab0/Lab0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab0
+//
+//  Created by Brendan S Chang on 2/8/16.
+//  Copyright © 2016 Brendan S Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
